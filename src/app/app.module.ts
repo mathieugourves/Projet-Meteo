c095b4cb237a103018b459c376af54cbfb1d26d6
Eilee Ryhuo <quentin.vallette@hotmail.com>
2018-02-09 15:33:11 +0100

    Routing + components accueil,connexion,navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
-
+const appRoutes: Routes = [
+  { path: '', component: AccueilComponent },
+  { path: 'connexion', component: ConnexionComponent },
+  { path: 'accueil',component: AccueilComponent },
+  /*{
+    path: 'heroes',
+    component: HeroListComponent,
+    data: { title: 'Heroes List' }
+  },
+  { path: '',
+    redirectTo: '/heroes',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }*/
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConnexionComponent,
+    NavigationComponent,
+    AccueilComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
