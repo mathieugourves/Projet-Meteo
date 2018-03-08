6c5f466f6cbc41e6a621da1d81833d862339771c
Mathieu GOURVES <math.gourves@gmail.com>
2018-03-08 16:10:09 +0100

    Gestion utilisateurs

--- server/connector.js
+++ server/connector.js
@@ -2,34 +2,60 @@ const mongoose = require('mongoose')
 const Music = require('./models/MusicModel.js')
 const Artist = require('./models/ArtistModel.js')
 const Album = require('./models/AlbumModel.js')
+const User = require('./models/UserModel.js')
 
-module.exports = function (host, port, dbName, options) {
-    this.host = host
-    this.dbName = dbName
-    this.port = port
-    this.options = options
+module.exports = class MongoConnector {
 
-    var self = this
+    constructor(host, port, dbName, options) {
+        this.host = host
+        this.dbName = dbName
+        this.port = port
+        this.options = options
 
-    var credentials = this.options.user + ':' + this.options.pass
-    var address = this.host + ':' + this.port
-    var options = 'authSource=' + this.options.authSource + '&poolSize=5'
-    var uri = 'mongodb://' + credentials + '@' + address + '/' + this.dbName + '?' + options
+        var credentials = this.options.user + ':' + this.options.pass
+        var address = this.host + ':' + this.port
+        var options = 'authSource=' + this.options.authSource + '&poolSize=5'
+        var uri = 'mongodb://' + credentials + '@' + address + '/' + this.dbName + '?' + options
 
-    mongoose.connect(uri)
+        mongoose.connect(uri)
+    }
 
-    this.getMusics = () => Music.find({}).populate('artist').populate('album').exec()
+    addUser(login, password, date) {
+        return User.create({
+            login: login,
+            password: password,
+            date: date
+        })
+    }
 
-    this.getArtists = () => Artist.find({}).exec()
+    getUser(login) {
+        return User.find({
+            login: login
+        }).exec()
+    }
 
-    this.getAlbums = () => Album.find({}).exec()
+    getMusics() {
+        return Music.find({}).populate('artist').populate('album').exec()
+    }
 
-    this.getMusicsByArtist = (id) => Music.find({
-        artist: mongoose.Types.ObjectId(id)
-    }).exec()
+    getArtists() {
+        return Artist.find({}).exec()
+    }
 
-    this.getMusicsByAlbum = (id) => Music.find({
-        album: mongoose.Types.ObjectId(id)
-    }).exec()
+    getAlbums() {
+        return Album.find({}).exec()
+    }
+
+    getMusicsByArtist(id) {
+        return Music.find({
+            artist: mongoose.Types.ObjectId(id)
+        }).exec()
+    }
+
+    getMusicsByAlbum(id) {
+        return Music.find({
+            album: mongoose.Types.ObjectId(id)
+        }).exec()
+    }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,46 +8,44 @@ import { AlbumComponent } from './album/album.component';
 import { MusiqueComponent } from './musique/musique.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { AccueilComponent } from './accueil/accueil.component';
-import { ArtisteComponent } from './artiste/artiste.component';
 
-import {MusiqueService} from './services/musique/musique.service';
-import {ArtisteService} from './services/artiste/artiste.service';
+import { MusiqueService } from './services/musique/musique.service';
+import { ArtisteService } from './services/artiste/artiste.service';
 import { ProfileArtisteComponent } from './artiste/profile-artiste/profile-artiste.component';
 import { ListArtisteComponent } from './artiste/list-artiste/list-artiste.component';
 const appRoutes: Routes = [
-  { path: 'connexion', component: ConnexionComponent },
-  { path: 'navigation', component: NavigationComponent },
-  { path: 'album',component: AlbumComponent },
-  { path: 'musique',component: MusiqueComponent },
-  { path: 'accueil',component: AccueilComponent },
-  { path: 'listArtiste',component: ListArtisteComponent },
-  {path : 'profilArtiste', component:ProfileArtisteComponent}
-  //A laisser en dernier car chemin par défaut
-  { path: '**', component: AccueilComponent }
+    { path: 'connexion', component: ConnexionComponent },
+    { path: 'navigation', component: NavigationComponent },
+    { path: 'album', component: AlbumComponent },
+    { path: 'musique', component: MusiqueComponent },
+    { path: 'accueil', component: AccueilComponent },
+    { path: 'listArtiste', component: ListArtisteComponent },
+    { path: 'profilArtiste', component: ProfileArtisteComponent },
+    //A laisser en dernier car chemin par défaut
+    { path: '**', component: AccueilComponent }
 ];
 @NgModule({
-  declarations: [
-    AppComponent,
-    ConnexionComponent,
-	NavigationComponent,
-    AlbumComponent,
-    MusiqueComponent,
-    AccueilComponent,
-    ArtisteComponen,
-    ProfileArtisteComponent,
-    ListArtisteComponentt
-  ],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
-    )
-  ],
-  providers: [
-      MusiqueService,
-      ArtisteService
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ConnexionComponent,
+        NavigationComponent,
+        AlbumComponent,
+        MusiqueComponent,
+        AccueilComponent,
+        ProfileArtisteComponent,
+        ListArtisteComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(
+            appRoutes,
+            { enableTracing: true } // <-- debugging purposes only
+        )
+    ],
+    providers: [
+        MusiqueService,
+        ArtisteService
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
