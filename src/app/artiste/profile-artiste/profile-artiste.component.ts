1468ea5918ad1dc9c354e069358e0f5f5ab5babc
Mathieu GOURVES <math.gourves@gmail.com>
2018-03-14 11:55:50 +0100

    Fix de albums by artist

--- src/app/artiste/profile-artiste/profile-artiste.component.ts
+++ src/app/artiste/profile-artiste/profile-artiste.component.ts
@@ -1,40 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import {ArtisteService} from 'app/services/artiste/artiste.service';
-import {MusiqueService} from 'app/services/musique/musique.service';
-import {AlbumService} from 'app/services/album/album.service';
-import {Artiste} from 'bean/artiste';
-import {Musique} from 'bean/musique';
-import {Album} from 'bean/album';
+import { ArtisteService } from 'app/services/artiste/artiste.service';
+import { MusiqueService } from 'app/services/musique/musique.service';
+import { AlbumService } from 'app/services/album/album.service';
+import { Artiste } from 'bean/artiste';
+import { Musique } from 'bean/musique';
+import { Album } from 'bean/album';
 
 @Component({
-  selector: 'app-profile-artiste',
-  templateUrl: './profile-artiste.component.html',
-  styleUrls: ['./profile-artiste.component.css']
+    selector: 'app-profile-artiste',
+    templateUrl: './profile-artiste.component.html',
+    styleUrls: ['./profile-artiste.component.css']
 })
 export class ProfileArtisteComponent implements OnInit {
-    artisteList : Artiste[];
-    selectedArtist : Artiste;
-    musicList : Musique[];
-    albumList : Album[]
-    constructor(private artisteService : ArtisteService,private musicService :MusiqueService,private albumService : AlbumService) { }
+    artisteList: Artiste[];
+    selectedArtist: Artiste;
+    musicList: Musique[];
+    albumList: Album[]
+    constructor(private artisteService: ArtisteService, private musicService: MusiqueService, private albumService: AlbumService) { }
 
- async ngOnInit() {
-      var vm = this;
-      var response = await this.artisteService.getAllArtists().then((response)=>{
-          vm.artisteList = response;
-      });
-      response = await this.musicService.getAllMusiques().then((response)=>{
-          vm.musicList = response;
-      });
-  }
-  //Fonction load les musiques d'un artiste
-  //mettre en async await
-  async onSelectChange(){
-      var vm = this;
-        var response = await this.albumService.getAlbumsByArtist(this.selectedArtist.id).then((response)=>{
-            vm.albumList  =response;
+    async ngOnInit() {
+        var vm = this;
+        var response = await this.artisteService.getAllArtists().then((response) => {
+            vm.artisteList = response;
+        });
+        response = await this.musicService.getAllMusiques().then((response) => {
+            vm.musicList = response;
+        });
+    }
+    //Fonction load les musiques d'un artiste
+    //mettre en async await
+    async onSelectChange() {
+        var vm = this;
+        var response = await this.artisteService.getAlbumsByArtist(this.selectedArtist.id).then((response) => {
+            vm.albumList = response;
         })
         console.log(this.albumList)
-  }
+    }
 
 }
--- src/app/services/artiste/artiste.service.ts
+++ src/app/services/artiste/artiste.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'environments/environment';
 import { Artiste } from 'bean/artiste'
+import { Album } from 'bean/album'
 
 @Injectable()
 export class ArtisteService {
